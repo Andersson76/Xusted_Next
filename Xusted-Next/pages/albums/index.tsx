378dade1409f477fb17c2715d52ab99fbacca720
Martin Andersson <martin.andersson@me.com>
2024-06-18 15:10:06 +0200

    Fixing the single album page

--- Xusted-Next/pages/albums/index.tsx
+++ Xusted-Next/pages/albums/index.tsx
@@ -29,14 +29,14 @@ export default function Albums() {
   return (
     <div className={`mt-40`}>
       <h1
-        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xlfont-bold text-gray-700 text-center ${inter.className}`}
+        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xlfont-bold text-gray-400 text-center ${inter.className}`}
       >
         EP & Singles
       </h1>
       <main className="flex justify-center">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
           {albums.map((album) => (
-            <li key={album.id} className="relative group flex justify-center">
+            <li key={album.id} className="relative group flex justify-center ">
               <Link href={`/albums/${album.id}`} passHref legacyBehavior>
                 <a className="relative block">
                   <Image
@@ -46,9 +46,6 @@ export default function Albums() {
                     height={200}
                     className="rounded-md mb-2 shadow-xl p-2 sm:p-4 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 transition-transform duration-300 transform group-hover:scale-90"
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <span className="text-white text-lg font-bold">Press</span>
-                  </div>
                   <div className="absolute bottom-2 right-2 bg-white rounded-full p-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 md:opacity-0 md:group-hover:opacity-100">
                     <FiArrowRight className="text-black" size={24} />
                   </div>
