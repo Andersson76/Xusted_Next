6c7b4e7d83e50775043d2a33862769d694e12498
Martin Andersson <martin.andersson@me.com>
2024-07-02 21:49:00 +0200

    Wrapping

--- Xusted-Next/components/BackgroundCircles.tsx
+++ Xusted-Next/components/BackgroundCircles.tsx
@@ -7,28 +7,26 @@ type Props = {}
 // eslint-disable-next-line no-empty-pattern
 function BackgroundCircles({}: Props) {
   return (
-    <div className="relative flex justify-center items-center overflow-hidden h-screen">
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          scale: [1, 2, 2, 3, 1],
-          opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
-          borderRadius: ['20%', '20%', '50%', '80%', '20%'],
-        }}
-        transition={{
-          duration: 0.5,
-        }}
-        className="absolute flex justify-center items-center"
-      >
-        <div className="absolute border border-[#333333] rounded-full h-52 w-52 animate-ping" />
-        <div className="rounded-full border border-[#F7AB0A] h-80 w-80 absolute opacity-10 animate-pulse" />
-        <div className="rounded-full border border-[#333333] h-130 w-130 absolute opacity-80 animate-ping" />
-        <div className="rounded-full border border-[#F7AB0A] opacity-20 h-170 w-170 absolute animate-pulse" />
-        <div className="rounded-full border border-[#333333] h-200 w-200 absolute" />
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ['20%', '20%', '50%', '80%', '20%'],
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+      className="absolute inset-0 flex justify-center items-center overflow-hidden"
+    >
+      <div className="absolute border border-[#333333] rounded-full h-52 w-52 animate-ping" />
+      <div className="rounded-full border border-[#F7AB0A] h-80 w-80 absolute opacity-10 animate-pulse" />
+      <div className="rounded-full border border-[#333333] h-130 w-130 absolute opacity-80 animate-ping" />
+      <div className="rounded-full border border-[#F7AB0A] opacity-20 h-170 w-170 absolute animate-pulse" />
+      <div className="rounded-full border border-[#333333] h-200 w-200 absolute" />
+    </motion.div>
   )
 }
 
--- Xusted-Next/pages/index.tsx
+++ Xusted-Next/pages/index.tsx
@@ -1,12 +1,7 @@
-'use client'
-
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import React from 'react'
-//import { Inter } from 'next/font/google'
 import BackgroundCircles from 'components/BackgroundCircles'
 import Buttons from 'components/Buttons'
-
-//const inter = Inter({ subsets: ['latin'] })
 
 const HomePage: React.FC = () => {
   const [text] = useTypewriter({
@@ -14,8 +9,9 @@ const HomePage: React.FC = () => {
     loop: true,
     delaySpeed: 3000,
   })
+
   return (
-    <div className="relative">
+    <div className="wrapper">
       <BackgroundCircles />
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         <h1 className="text-2xl lg:text-6xl font-semibold px-10 text-center text-white text-opacity-60">
@@ -29,4 +25,5 @@ const HomePage: React.FC = () => {
     </div>
   )
 }
+
 export default HomePage
