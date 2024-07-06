31ea1b42434bbe2dc5e8c2d8d782a348e97139d4
Martin Andersson <martin.andersson@me.com>
2024-07-06 23:11:51 +0200

    Wrapping up contact page

--- Xusted-Next/pages/contact/index.tsx
+++ Xusted-Next/pages/contact/index.tsx
@@ -31,96 +31,98 @@ export default function Contact() {
   }
 
   return (
-    <motion.div
-      className="flex flex-col items-center justify-center p-5 mt-32"
-      initial={{ x: -500 }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', stiffness: 50 }}
-    >
-      <Formik
-        initialValues={{ name: '', email: '', message: '' }}
-        validationSchema={Yup.object({
-          name: Yup.string().required('Required'),
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('Required'),
-          message: Yup.string().required('Required'), // make sure message is required
-        })}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          sendEmail(values)
-          setSubmitting(false)
-          resetForm()
-        }}
+    <div className="wrapper">
+      <motion.div
+        className="flex flex-col items-center justify-center p-5 mt-20"
+        initial={{ x: -500 }}
+        animate={{ x: 0 }}
+        transition={{ type: 'spring', stiffness: 50 }}
       >
-        {({ errors, touched, isSubmitting }) => (
-          <Form className="flex flex-col gap-4 w-full max-w-lg mx-auto p-5 border border-gray-200 rounded-lg shadow-md bg-gray-200">
-            <h3 className="text-xl font-bold mb-5 text-center text-black opacity-75">
-              Contact
-            </h3>
-            <div>
-              <label
-                htmlFor="name"
-                className="text-sm font-medium text-black opacity-80"
+        <Formik
+          initialValues={{ name: '', email: '', message: '' }}
+          validationSchema={Yup.object({
+            name: Yup.string().required('Required'),
+            email: Yup.string()
+              .email('Invalid email address')
+              .required('Required'),
+            message: Yup.string().required('Required'), // make sure message is required
+          })}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            sendEmail(values)
+            setSubmitting(false)
+            resetForm()
+          }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form className="flex flex-col gap-4 w-full max-w-lg mx-auto p-5 border border-gray-200 rounded-lg shadow-md bg-gray-200">
+              <h3 className="text-xl font-bold mb-5 text-center text-black opacity-75">
+                Contact
+              </h3>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="text-sm font-medium text-black opacity-80"
+                >
+                  Name
+                </label>
+                <Field
+                  type="text"
+                  name="name"
+                  className="w-full p-2 mt-1 border border-gray-300 rounded"
+                />
+                {errors.name && touched.name && (
+                  <div className="text-red-500 text-sm mt-1">
+                    <ErrorMessage name="name" />
+                  </div>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="text-sm font-medium text-black opacity-80"
+                >
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  className="w-full p-2 mt-1 border border-gray-300 rounded"
+                />
+                {errors.email && touched.email && (
+                  <div className="text-red-500 text-sm mt-1">
+                    <ErrorMessage name="email" />
+                  </div>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="message"
+                  className="text-sm font-medium text-black opacity-80"
+                >
+                  Message
+                </label>
+                <Field
+                  as="textarea"
+                  name="message"
+                  className="w-full p-2 mt-1 border border-gray-300 rounded h-28"
+                />
+                {errors.message && touched.message && (
+                  <div className="text-red-500 text-sm mt-1">
+                    <ErrorMessage name="message" />
+                  </div>
+                )}
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="p-3 text-white bg-blue-600 rounded hover:bg-blue-700"
               >
-                Name
-              </label>
-              <Field
-                type="text"
-                name="name"
-                className="w-full p-2 mt-1 border border-gray-300 rounded"
-              />
-              {errors.name && touched.name && (
-                <div className="text-red-500 text-sm mt-1">
-                  <ErrorMessage name="name" />
-                </div>
-              )}
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="text-sm font-medium text-black opacity-80"
-              >
-                Email
-              </label>
-              <Field
-                type="email"
-                name="email"
-                className="w-full p-2 mt-1 border border-gray-300 rounded"
-              />
-              {errors.email && touched.email && (
-                <div className="text-red-500 text-sm mt-1">
-                  <ErrorMessage name="email" />
-                </div>
-              )}
-            </div>
-            <div>
-              <label
-                htmlFor="message"
-                className="text-sm font-medium text-black opacity-80"
-              >
-                Message
-              </label>
-              <Field
-                as="textarea"
-                name="message"
-                className="w-full p-2 mt-1 border border-gray-300 rounded h-28"
-              />
-              {errors.message && touched.message && (
-                <div className="text-red-500 text-sm mt-1">
-                  <ErrorMessage name="message" />
-                </div>
-              )}
-            </div>
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="p-3 text-white bg-blue-600 rounded hover:bg-blue-700"
-            >
-              Send
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </motion.div>
+                Send
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </motion.div>
+    </div>
   )
 }
