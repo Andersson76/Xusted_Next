cd9c1da99fa9adfdf1529e531adb5a6738b58791
Martin Andersson <martin.andersson@me.com>
2024-07-09 22:51:47 +0200

    Rendering song

--- Xusted-Next/components/Album.tsx
+++ Xusted-Next/components/Album.tsx
@@ -1,36 +1,39 @@
 'use client'
 
 import React from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
+interface Song {
+  title: string
+  duration: string
+}
 interface AlbumProps {
   id: string
   title: string
   cover: string
-  songs: string[]
+  songs: Song[]
 }
 
-const Album: React.FC<AlbumProps> = ({ id, title, cover, songs }) => {
+const Album: React.FC<AlbumProps> = ({ title, cover, songs }) => {
+  console.log('Album songs data:', songs)
+
   return (
-    <div className="wrapper">
-      <div className="grid place-items-center text-center min-h-[60vh] mt-24">
-        <Link href={`/albums/${id}`} passHref legacyBehavior>
-          <a>
-            <Image
-              src={cover}
-              alt={title}
-              width={200}
-              height={200}
-              className="mb-2 p-2 sm:p-4 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80"
-            />
-            <h2 className="text-white text-opacity-75 text-2xl">{title}</h2>
-            <ul className="text-white text-opacity-75 mt-5">
-              {songs.map((song, index) => (
-                <li key={index}>{song}</li>
-              ))}
-            </ul>
-          </a>
-        </Link>
+    <div className="wrapper text-white text-opacity-75">
+      <div className="flex flex-col items-center text-center min-h-[60vh] mt-40">
+        <Image
+          src={cover}
+          alt={title}
+          width={200}
+          height={200}
+          className="mb-2 p-2 sm:p-4 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80"
+        />
+        <h2 className="text-2xl font-bold mb-2">{title}</h2>
+        <ul className="mt-5">
+          {songs.map((song, index) => (
+            <li key={index} className="hover:bg-gray-700 px-4 py-2">
+              {song}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
--- Xusted-Next/pages/_app.tsx
+++ Xusted-Next/pages/_app.tsx
@@ -7,6 +7,7 @@ import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Footer from '../components/Footer'
+//import AlbumList from 'components/AlbumList'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
